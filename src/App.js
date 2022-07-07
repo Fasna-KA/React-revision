577c967e1ee4b0e131c87ac902b5af9801f735c3
FASNA-KA <jasmeeazeez813@gmail.com>
2022-07-07 19:03:59 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Effect from './day3/UseEffect';
 // import ClassComp from "./day1/classComp"
 // import FunctionalComp from "./day1/functionalComp"
-import Nav from './day2/Nav'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './day2/pages/Home';
-import About from './day2/pages/About';
-import Notfound from './day2/pages/Notfound';
+
+// import Nav from './day2/Nav'
+// import { BrowserRouter, Routes, Route} from 'react-router-dom'
+// import Home from './day2/pages/Home';
+// import About from './day2/pages/About';
+// import Notfound from './day2/pages/Notfound';
+
+import Reducer from './day3/UseReducer'
+import State from './day3/UseState';
+import EffectHook from './day3/UseEffect';
 
 function App() {
   return (
@@ -14,14 +20,18 @@ function App() {
       {/* <FunctionalComp/>
       <ClassComp/> */}
 
-<BrowserRouter>
+{/* <BrowserRouter>
   <Nav/>
   <Routes>
   <Route path='/home' element={<Home/>}/>
   <Route path='/about' element={<About/>}/>
   <Route path='/notfound' element={<Notfound/>}/>
   </Routes>
-</BrowserRouter>
+</BrowserRouter> */}
+
+  <State/>
+  <Reducer/>
+  <EffectHook/>
 
 
     </div>
